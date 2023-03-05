57a127d5b967f5417e0d419b113f2c258c961062
sw.park <sw.park@birdview.kr>
2023-03-06 01:05:35 +0900

    updated

--- lib/services/api_services.dart
+++ lib/services/api_services.dart
@@ -2,6 +2,8 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 import 'package:toonflix/models/webtoon.dart';
+import 'package:toonflix/models/webtoon_detail.dart';
+import 'package:toonflix/models/webtoon_episode.dart';
 
 class ApiService {
   static const String baseUrl =
@@ -11,6 +13,7 @@ class ApiService {
     List<WebtoonModel> webtoonInstances = [];
     Uri url = Uri.parse('$baseUrl/today');
     final response = await http.get(url);
+
     if (response.statusCode == 200) {
       final List<dynamic> webtoons = jsonDecode(response.body);
       for (var webtoon in webtoons) {
@@ -18,6 +21,31 @@ class ApiService {
         webtoonInstances.add(webtoonModel);
       }
       return webtoonInstances;
+    }
+    throw Error();
+  }
+
+  static Future<WebtoonDetailModel> getWebtoonById(String id) async {
+    Uri url = Uri.parse('$baseUrl/$id');
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      return WebtoonDetailModel.fromJson(jsonDecode(response.body));
+    }
+    throw Error();
+  }
+
+  static Future<List<WebtoonEpisode>> getWebtoonEpisodesById(String id) async {
+    List<WebtoonEpisode> webtoonEpisodeInstances = [];
+    Uri url = Uri.parse('$baseUrl/$id/episodes');
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final List<dynamic> episodes = jsonDecode(response.body);
+      for (var episode in episodes) {
+        var episodeModel = WebtoonEpisode.fromJson(episode);
+        webtoonEpisodeInstances.add(episodeModel);
+      }
+      return webtoonEpisodeInstances;
     }
     throw Error();
   }

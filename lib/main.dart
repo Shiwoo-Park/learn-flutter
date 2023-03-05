7320ef10eded2ebad1ca825cd32e897563f7d7b8
sw.park <sw.park@birdview.kr>
2023-03-06 00:47:53 +0900

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:toonflix/screens/home_screen.dart';
-import 'package:toonflix/screens/webtoon/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
